e9d162868875b5f5166eef254d8d03b105e9a4ef
stan581994 <stan581994@gmail.com>
2024-06-15 12:05:16 +0800

    finish week 1

---
+++ scripts/getDates.js
@@ -0,0 +1,33 @@
+function setFooterDates() {
+    const footer = document.querySelector('footer');
+    // Create paragraph elements for the dates
+    const currentYearPara = document.createElement('p');
+    const lastModifiedPara = document.createElement('p');
+
+    // Get the current year
+    const currentYear = new Date().getFullYear();
+    currentYearPara.innerHTML = `©️ ${currentYear} ⭐ Steven Tan  ⭐ Philippines`;
+
+    // Get the last modified date
+    const lastModified = document.lastModified;
+    lastModifiedPara.textContent = `Last Modified: ${lastModified}`;
+
+    // Create an image element for the flag
+    const flagImg = document.createElement('img');
+    flagImg.src = 'images/ph_flag.png';
+    flagImg.width = '75';
+    flagImg.height = '50';
+
+    // Append the paragraphs to the footer
+    footer.appendChild(currentYearPara);
+    footer.appendChild(flagImg);
+
+
+    // Create a new paragraph for last modified date
+    const lastModifiedContainer = document.createElement('p');
+    lastModifiedContainer.appendChild(lastModifiedPara);
+    footer.appendChild(lastModifiedContainer);
+
+}
+    // Run the function once the DOM is fully loaded
+    document.addEventListener('DOMContentLoaded', setFooterDates);

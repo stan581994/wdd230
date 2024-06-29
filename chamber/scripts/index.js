7a1d8f5a62acfd8c2bc3158a74e8a218cec7ba4d
steven-tan-telus <steven.tan@telus.com>
2024-06-29 10:27:52 -0400

    initial fix

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -1,1 +1,11 @@
 document.getElementById('lastModified').textContent = new Date(document.lastModified).toLocaleDateString();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav');
+
+    hamburger.addEventListener('click', function() {
+        this.classList.toggle('open');
+        navMenu.classList.toggle('showMenu');
+    });
+});

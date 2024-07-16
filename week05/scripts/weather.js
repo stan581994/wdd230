c78d0afb2d060c81bacd51b731a21c727e1df248
steven-tan-telus <steven.tan@telus.com>
2024-07-16 06:48:22 -0400

    week 5 completed

---
+++ week05/scripts/weather.js
@@ -0,0 +1,32 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=2295b521a6bb77f339b871e2c1d1c434';
+
+async function apiFetch(){
+    try{
+        const response = await fetch(url);
+        if (response.ok){
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }else{
+            throw Error(await response.text());
+        }
+
+    }catch(error){
+        console.error(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data){
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconUrl);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
